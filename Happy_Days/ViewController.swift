cf75f042fd4b03588da8f230c799aa8638682e8b
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-02-01 17:13:35 +1300

    initial

---
+++ Happy_Days/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happy_Days
+//
+//  Created by Daniel Wallace on 1/02/17.
+//  Copyright © 2017 Daniel Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
