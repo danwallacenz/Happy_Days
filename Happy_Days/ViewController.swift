03d36fd1c498d3b617e2b2dc1f2c9272e19ac46c
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-02-04 00:21:05 +1300

    permissions set up

--- Happy_Days/ViewController.swift
+++ Happy_Days/ViewController.swift
@@ -7,13 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
+import Speech
+
+/*
+ see info.plist
+ 
+ NSPhotoLibraryUsageDescription = “We use this to let you import photos”
+ NSMicrophoneUsageDescription = “We use this to record your narration”
+ NSSpeechRecognitionUsageDescription = “We use this to transcribe your narration”
+*/
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var helpLabel: UILabel!
-    
-    
+ 
     @IBAction func requestPermissions(_ sender: Any) {
+        requestPhotosPermissions()
+    }
+
+    private func requestPhotosPermissions() {
+        PHPhotoLibrary.requestAuthorization { [unowned self] authStatus in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    self.requestRecordPermissions()
+                } else {
+                    self.helpLabel.text = "Photos permission was declined; please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    }
+    
+    private func requestRecordPermissions() {
+        AVAudioSession.sharedInstance().requestRecordPermission() { [unowned self] allowed in
+            DispatchQueue.main.async {
+                if allowed {
+                    self.requestTranscribePermissions()
+                } else {
+                    self.helpLabel.text = "Recording permission was declined; please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    }
+
+    private func requestTranscribePermissions() {
+        SFSpeechRecognizer.requestAuthorization { [unowned self] authStatus in
+            DispatchQueue.main.async {
+                if authStatus == .authorized {
+                    self.authorizationComplete()
+                } else {
+                    self.helpLabel.text = "Transcription permission was declined; please enable it in settings then tap Continue again."
+                }
+            }
+        }
+    }
+    
+    private func authorizationComplete() {
+        dismiss(animated: true)
     }
     
     override func viewDidLoad() {
