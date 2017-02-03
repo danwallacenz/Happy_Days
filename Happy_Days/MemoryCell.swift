1ffa97fdb855bd9cee747e630d16666cdff4db51
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-02-03 23:33:55 +1300

    ui set up

---
+++ Happy_Days/MemoryCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemoryCell.swift
+//  Happy_Days
+//
+//  Created by Daniel Wallace on 3/02/17.
+//  Copyright © 2017 Daniel Wallace. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+   
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
