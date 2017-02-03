cd8c6fb5b9f1373e0d0c13ac5cde35044c5353dd
Dan Wallace <daniel.wallace.nz@gmail.com>
2017-02-04 00:58:43 +1300

    set up uicollection vc data from files

--- Happy_Days/MemoriesViewController.swift
+++ Happy_Days/MemoriesViewController.swift
@@ -13,15 +13,52 @@ import Speech
 
 class MemoriesViewController: UICollectionViewController {    
     
+    private var memories = [URL]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadMemories()
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         checkPermissions()
     }
     
+    private func loadMemories() {
+        memories.removeAll()
+        
+        // attempt to load all the memories in our documents directory
+        guard let files = try? FileManager.default.contentsOfDirectory(at: getDocumentsDirectory(), includingPropertiesForKeys: nil, options: []) else { return }
+        
+         // loop over every file found
+        for file in files {
+            let fileName = file.lastPathComponent
+            
+            // check it ends with ".thumb" so we don't count each memory more than once
+            if fileName.hasSuffix(".thumb") {
+                
+                // get the root name of the memory (i.e., without its path extension)
+                let noExtension = fileName.replacingOccurrences(of: ".thumb", with: "")
+                
+                // create a full path from the memory
+                let memoryPath = getDocumentsDirectory().appendingPathComponent(noExtension)
+                
+                // add it to our array
+                memories.append(memoryPath)
+            }
+        }
+        // section 0 is the search bar
+        collectionView?.reloadSections(IndexSet(integer: 1))
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+    
     private func checkPermissions() {
+        
         // check status for all three permissions
         let photosAuthorized = PHPhotoLibrary.authorizationStatus() == .authorized
         let recordingAuthorized = AVAudioSession.sharedInstance().recordPermission() == .granted
